d35fd5752e5d1c8e7919265959baeaaadea628a7
Travis-CI <comindware-awesome-b@comindware.com>
2018-05-21 15:15:56 +0000

    Auto-deploy to Github Pages

---
+++ precache-manifest.084e2937cbb5ea349ce5c629a169b8b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "node_modules/font-awesome/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "node_modules/font-awesome/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "node_modules/font-awesome/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "node_modules/font-awesome/fonts/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "node_modules/font-awesome/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "28d3b3a3e949f9fb98c8e2431e1bf417",
+    "url": "index.html"
+  },
+  {
+    "revision": "532530bc30ea5f4e1935",
+    "url": "app.js"
+  },
+  {
+    "revision": "532530bc30ea5f4e1935",
+    "url": "app.css"
+  }
+];
